bcb18b401a99e05676c2bce108809985f70a33d1
Anastasiia <anasta_ok@mail.ru>
2022-09-16 17:22:49 +0300

    added kisa cart

--- src/pages/KisaCart.tsx
+++ src/pages/KisaCart.tsx
@@ -1,5 +1,15 @@
+import Cart from "../components/Cart";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
 const KisaCart = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Cart />
+      <Footer />
+    </div>
+  );
 };
 
 export default KisaCart;
