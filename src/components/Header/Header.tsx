1958de22b26b9bf2cb10c3255af686c2aac46d6f
Anastasiia <anasta_ok@mail.ru>
2022-10-27 19:10:41 +0300

    added mobile adaptive menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,19 @@
 import { NavLink } from "react-router-dom";
 import { Link } from "react-scroll";
+import { useState } from "react";
 
 import "./Header.scss";
 
 import { FaCat } from "react-icons/fa";
 import { GiBasket } from "react-icons/gi";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Header = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => {
+    setClick(!click);
+  };
+  const closeMobileMenu = () => setClick(false);
   return (
     <div className="header">
       <div className="logo">
@@ -21,29 +28,37 @@ const Header = () => {
         </NavLink>
       </div>
       <div className="containerMenu">
-        <ul className="navMenu">
-          <li className="category">
+        <ul className={click ? "navMenu active" : "navMenu"}>
+          <li className="category" onClick={closeMobileMenu}>
             <NavLink className="categoryLink" to="/catalog">
               Выбрать друга
             </NavLink>
           </li>
-          <li className="category">
+          <li className="category" onClick={closeMobileMenu}>
             <NavLink className="categoryLink" to="/help">
               Помощь кисам
             </NavLink>
           </li>
-          <li className="category">
+          <li className="category" onClick={closeMobileMenu}>
             <Link className="categoryLink" to="footer" smooth={true}>
               Контакты
             </Link>
           </li>
+          <NavLink className="link" to="/cart">
+            <li className="cartWrapper" onClick={closeMobileMenu}>
+              Корзинка
+              <GiBasket className="iconCart" />
+            </li>
+          </NavLink>
         </ul>
-        <NavLink className="link" to="/cart">
-          <div className="cartWrapper">
-            Корзинка
-            <GiBasket className="iconCart" />
-          </div>
-        </NavLink>
+
+        <div className="hamburger" onClick={handleClick}>
+          {click ? (
+            <FaTimes className="hamburgerIcon" />
+          ) : (
+            <FaBars className="hamburgerIcon" />
+          )}
+        </div>
       </div>
     </div>
   );
