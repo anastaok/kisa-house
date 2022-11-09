ee9521ec5aa988923535849f99f7a6dd26c8255d
Anastasiia <anasta_ok@mail.ru>
2022-11-09 17:51:25 +0300

    update Cart, InfoKisaPage and KisaTake styles

--- src/components/KisaTake/KisaTake.jsx
+++ src/components/KisaTake/KisaTake.jsx
@@ -1,11 +1,21 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../Button";
 import "./KisaTake.scss";
 
 const KisaTake = () => {
+  const navigate = useNavigate();
+  const backToHome = () => navigate("/");
+
   return (
     <div className="kisaTake">
       <p className="titleKisaTake">
-        Отлично, все данные отправлены! Скоро с Вами свяжется наш помощник и
-        объяснит дальнейшие действия.
+        Отлично, все данные отправлены!
+        <p className="titleKisaTake">
+          Скоро с Вами свяжется наш помощник и объяснит дальнейшие действия.
+        </p>
+        <Button width="lg" onClick={backToHome}>
+          {"Вернуться на главную"}
+        </Button>
       </p>
     </div>
   );
